3f4810dfc46681e92e5bbf56a29589098fffdea0
Dimash <dinmukhamed2517@gmail.com>
2023-10-20 12:01:46 +0600

    initial commit

---
+++ app/src/main/java/kz/just_code/lab2room/Word.kt
@@ -0,0 +1,9 @@
+package kz.just_code.lab2room
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "word_table")
+class Word(@PrimaryKey @ColumnInfo(name="word") val word:String)
